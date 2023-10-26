054693cdbac8e72c00fd6f431203991c619c3933
imostafao <138694850+imostafao@users.noreply.github.com>
2023-10-26 23:23:48 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-int get_bit(unsigned long int x, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int hold;
 
 	if (index > 64)
 		return (-1);
 
-	hold = x >> index;
+	hold = n >> index;
 
 	return (hold & 1);
 }
