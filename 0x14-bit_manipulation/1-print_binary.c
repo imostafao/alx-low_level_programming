e79d945c57128468090f5b9fddfe0b22232b35f0
imostafao <138694850+imostafao@users.noreply.github.com>
2023-10-26 23:32:43 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_binary - print binary representation of a number
+ * @n: decimal number to print as binary
+ */
+
 void print_binary(unsigned long int n)
 {
 	unsigned long int temp;
