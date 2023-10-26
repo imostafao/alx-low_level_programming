fd7ea28a1156861859b133c7e49e18008881e3ba
imostafao <138694850+imostafao@users.noreply.github.com>
2023-10-26 21:34:43 +0300

    Create 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int total, power;
+	int len;
+
+	if (a == NULL)
+		return (0);
+
+	for (len = 0; a[len]; len++)
+	{
+		if (a[len] != '0' && a[len] != '1')
+			return (0);
+	}
+
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (a[len] == '1')
+			total += power;
+	}
+
+	return (total);
+}
